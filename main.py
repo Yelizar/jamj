5adf0822bb9338f58f0947f4c7b5b71df028db94
VldFrlv <frolov360180@gmail.com>
2021-12-08 22:35:09 +0200

    frolov first commint

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+pygame.init()
+
+window = pygame.display.set_mode((400, 650)) # screen object(width,height)
+pygame.display.set_caption('jamj')
+
+run = True
+
+while run:
+    keys = pygame.key.get_pressed()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    if keys [pygame.K_ESCAPE]:
+        run = False
+
+
+
+
+
+
+
+
+
+
